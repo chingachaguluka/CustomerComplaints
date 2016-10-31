53e37fbd96a9bddea480c6516b38c898f30c5ce9
Chinga Chaguluka <chinga.e.chaguluka@gmail.com>
2016-10-31 22:08:33 +0200

    Create CardList and CardDetails components

---
+++ src/components/card-details.js
@@ -0,0 +1,15 @@
+import { Component }, React from 'react';
+
+class CardDetails extends Component {
+
+    render(){
+
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default CardDetails;
---
+++ src/components/card-list.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import CardDetails from './card-details';
+
+
+class CardList extends Component {
+
+    render() {
+
+        return (
+            <div>
+                Card Listing works!!
+            </div>
+        );
+    }
+}
+
+export default CardList;
