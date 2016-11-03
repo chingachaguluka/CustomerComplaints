2f0603c806f9add4ab780ce1fd40f112c7ccd7ec
Chinga <royalse-me@yahoo.com>
2016-11-03 17:22:19 +0200

    Update complaint list to display complaints

--- src/containers/complaint-list.js
+++ src/containers/complaint-list.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
-import ComplaintDetails from './complaint-details';
 import { connect } from 'react-redux';
+import ComplaintDetails from './complaint-details';
+import ComplaintListItem from '../components/complaint-list-item';
 
 class ComplaintList extends Component {
+    
+    renderList() {
+
+        return this.props.complaints.map((complaint) => {
+            return (
+                <a href="#" class="list-group-item" key={complaint.id}>
+                    <h4 class="list-group-item-heading">{complaint.customerName}  <span className="float-right">#{complaint.id}</span></h4>
+                    <p class="list-group-item-text">Branch: {complaint.branch}</p>
+                </a>
+            );
+        });
+    }
+
 
     render() {
 
         return (
             <div>
-                Card Listing works!!
+                <ul className="list-group">{this.renderList()}</ul>
             </div>
         );
     }
